c255ea17e3e71162d8fe1b9773b7467423c49da5
congquynguyen296 <congquynguyen296@gmail.com>
2025-05-25 21:12:02 +0700

    Client - categories

---
+++ client/src/components/BestSeller.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const BestSeller = () => {
+  return (
+    <div className="mt-16">
+      <p className="text-2xl md:text-3xl font-medium">Best Sellers</p>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6">
+        
+      </div>
+    </div>
+  );
+};
